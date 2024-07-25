abc377e55431347115a49d0455b7ebdc37c191d9
justinac0 <justinac.1999@gmail.com>
2024-07-25 22:47:39 +1000

    Fixed File Writing Race Condition

--- src/main.py
+++ src/main.py
@@ -5,21 +5,44 @@ import time
 from playsound3 import playsound
 
 from simulation import Simulation
+from file_data import FileData
 
-def simulate_on_single_core(NT: int, NP: list[int], D0: float, dt: float):
+import concurrent.futures
+
+def simulation_worker(i, NT, NP, D0, dt) -> FileData:
+    file_path = f'resources/data/{i}_bulk_water_{NT}_{NP}.txt'
+    contents = (f'{NP}> Simulation Index: {i+1}\n')
+
     start = time.time()
-    Simulation.run_sequential(NT, NP, D0, dt)
+    contents += (Simulation.static_run(NT, NP, D0, dt))
     end = time.time()
 
-    file = open(f'resources/data/time_elapsed.txt', 'w')
-    file.write(f'time_elapsed: {end - start} seconds')
-    file.close()
+    contents += (f'time_elapsed: {end - start} seconds')
 
-    playsound('resources/audio/water_drop_reverb.wav')
+    return FileData(file_path, contents)
 
-def simulate_on_multiple_cores(NT: int, NP: list[int], D0: float, dt: float):
+def run_parallel(NT: int, NP: list[int], D0: float, dt: float):
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        files = []
+        futures = []
+
+        for particle_count in NP:
+            for i in range(0, 3): # running in triplicate
+                future = executor.submit(simulation_worker, i, NT, particle_count, D0, dt)
+                futures.append(future)
+
+        for f in concurrent.futures.as_completed(futures):
+            files.append(f.result())
+
+        concurrent.futures.wait(futures)
+
+        for file in files:
+            with open(file.file_path, 'w') as stream:
+                stream.write(file.contents)
+
+def simulate_on_multiple_cores(NT, NP, D0, dt):
     start = time.time()
-    Simulation.run_parallel(NT, NP, D0, dt)
+    run_parallel(NT, NP, D0, dt)
     end = time.time()
 
     file = open(f'resources/data/time_elapsed.txt', 'w')
@@ -33,7 +56,6 @@ if __name__ == '__main__':
     D0 = 2.3 * 10**(-3) # diffusion coefficient
     dt = 5 * 10**(-9)   # time step
 
-    NP = [10, 30, 100, 300, 1000, 3000, 10000, 30000, 100000, 300000]
+    NP = [10, 30, 100]
 
-    # simulate_on_single_core(NT, NP, D0, dt) # multiple cores almost always faster
     simulate_on_multiple_cores(NT, NP, D0, dt)
