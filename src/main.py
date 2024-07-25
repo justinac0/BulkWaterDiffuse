42ac5670aba79a007fd5fd3f501f003f054097de
justinac0 <justinac.1999@gmail.com>
2024-07-25 23:00:30 +1000

    Getting Ready to Plot

--- src/main.py
+++ src/main.py
@@ -9,36 +9,49 @@ from file_data import FileData
 
 import concurrent.futures
 
-def simulation_worker(i, NT, NP, D0, dt) -> FileData:
+def simulation_worker(i, NT, NP, D0, dt) -> tuple:
     file_path = f'resources/data/{i}_bulk_water_{NT}_{NP}.txt'
     contents = (f'{NP}> Simulation Index: {i+1}\n')
 
     start = time.time()
+    simulation = Simulation(NT, NP, D0, dt)
+    simulation_data = simulation.run().get()
+
+    _, diffusion_tensor, eigen_diffusion_tensor, fa = simulation_data
+
+    contents += (f'''Simulation Details:\nNT: {NT}\n
+                    NP: {NP}\nD0: {D0}\n
+                    dt: {dt}\n\n
+                    Diffusion Tensor:\n{diffusion_tensor}\n\n
+                    Diffusion Tensor Eigen Values:\n{eigen_diffusion_tensor}\n\n
+                    Fractional Anisotropy:\n{fa}\n''')
+
     contents += (Simulation.static_run(NT, NP, D0, dt))
     end = time.time()
 
-    contents += (f'time_elapsed: {end - start} seconds')
+    elapsed = (f'time_elapsed: {end - start} seconds')
+    print(f'FINISHED: {i}, {NT}, {NP}, {elapsed}')
+    contents += elapsed
 
-    return FileData(file_path, contents)
+    return (simulation_data, FileData(file_path, contents))
 
-def run_parallel(NT: int, NP: list[int], D0: float, dt: float):
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        files = []
+def run_parallel(NT: int, NP: list[int], D0: float, dt: float, repeats=1):
+    data = []
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
         futures = []
 
         for particle_count in NP:
-            for i in range(0, 3): # running in triplicate
+            for i in range(0, repeats):
                 future = executor.submit(simulation_worker, i, NT, particle_count, D0, dt)
                 futures.append(future)
 
         for f in concurrent.futures.as_completed(futures):
-            files.append(f.result())
+            data.append(f.result())
 
         concurrent.futures.wait(futures)
 
-        for file in files:
-            with open(file.file_path, 'w') as stream:
-                stream.write(file.contents)
+    return data
 
 def simulate_on_multiple_cores(NT, NP, D0, dt):
     start = time.time()
@@ -56,6 +69,10 @@ if __name__ == '__main__':
     D0 = 2.3 * 10**(-3) # diffusion coefficient
     dt = 5 * 10**(-9)   # time step
 
-    NP = [10, 30, 100]
+    NP = [10, 30, 100, 300, 1000, 3000, 10000, 30000]
 
-    simulate_on_multiple_cores(NT, NP, D0, dt)
+    _, files = simulate_on_multiple_cores(NT, NP, D0, dt)
+
+    for file in files:
+        with open(file.file_path, 'w') as stream:
+            stream.write(file.contents)
--- src/simulation.py
+++ src/simulation.py
@@ -1,10 +1,7 @@
-import time
-import concurrent.futures
 import numpy as np
 
 from sim_math import SimMath
 from particle import Particle
-from file_data import FileData
 
 class SimulationData:
     def __init__(self, particles: list[Particle], diffusion_tensor: list[list[float]], eigen_diffusion_tensor: list[float], fractional_anisotropy: float):
@@ -67,55 +64,3 @@ class Simulation:
         FA = SimMath.calculate_fractional_anisotropy(Dxx, Dyy, Dzz)
 
         return SimulationData(particles, DT, eigens, FA)
-
-    @staticmethod
-    def static_run(NT, NP, D0, dt) -> str:
-        simulation = Simulation(NT, NP, D0, dt)
-        _, diffusion_tensor, eigen_diffusion_tensor, fa = simulation.run().get()
-        return (f'Simulation Details:\nNT: {NT}\nNP: {NP}\nD0: {D0}\ndt: {dt}\n\nDiffusion Tensor:\n{diffusion_tensor}\n\nDiffusion Tensor Eigen Values:\n{eigen_diffusion_tensor}\n\nFractional Anisotropy:\n{fa}\n')
-
-    # @staticmethod
-    # def run_sequential(NT: int, NP: list[int], D0: float, dt: float):
-    #     for particle_count in NP:
-    #         for i in range(0, 3): # running in triplicate
-    #             # output information about current simulation to files...
-    #             file = open(f'resources/data/{i}_bulk_water_{NT}_{particle_count}.txt', 'w')
-    #             file.write(f'{particle_count}> Simulation Index: {i+1}\n')
-
-    #             start = time.time()
-    #             file.write(Simulation.static_run(NT, particle_count, D0, dt))
-    #             end = time.time()
-
-    #             file.write(f'time_elapsed: {end - start} seconds')
-    #             file.close()
-
-    # @staticmethod
-    # def run_parallel(NT: int, NP: list[int], D0: float, dt: float):
-    #     def worker(i: int, NT: int, NP: int, D0: float, dt: float, files: list):
-    #         file_path = f'resources/data/threaded/{i}_bulk_water_{NT}_{NP}.txt'
-    #         contents = (f'{NP}> Simulation Index: {i+1}\n')
-
-    #         start = time.time()
-    #         contents += (Simulation.static_run(NT, NP, D0, dt))
-    #         end = time.time()
-
-    #         contents += (f'time_elapsed: {end - start} seconds')
-
-    #         file = FileData(file_path, contents)
-    #         files.append(file)
-
-    #     files = []
-    #     with concurrent.futures.ProcessPoolExecutor() as executor:
-    #         futures = []
-
-    #         for particle_count in NP:
-    #             for i in range(0, 3): # running in triplicate
-    #                 future = executor.submit(worker, i, NT, particle_count, D0, dt, files)
-    #                 futures.append(future)
-
-    #         concurrent.futures.wait(futures)
-
-    #     for file in files:
-    #         with open(file.file_path, 'w') as stream:
-    #             stream.write(file.file_path + '\n')
-    #             stream.write(file.contents)
