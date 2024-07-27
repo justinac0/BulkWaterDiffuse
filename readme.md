a4c7d3f24681dd8539307c983685f6e09f22620a
justinac0 <justinac.1999@gmail.com>
2024-07-27 14:14:37 +1000

    Fixed Results

--- readme.md
+++ readme.md
@@ -5,10 +5,7 @@ WIP...
 WIP...
 
 # Showcase
-![Uniform Sampling of Unit Sphere](showcase/uniform_sampling.png)
-![Spherical Coordinants Theta Validation](showcase/theta_validation.png)
-![Spherical Coordinants Phi Validation](showcase/phi_validation.png)
-![Diffusion Of Bulk Water](showcase/diffusion.png)
+WIP...
 
 # References
 - Momot, K.I. Diffusion tensor of water in model articular cartilage. Eur Biophys J 40, 81–91 (2011). https://doi.org/10.1007/s00249-010-0629-4
--- src/main.py
+++ src/main.py
@@ -18,14 +18,9 @@ def simulation_worker(i: int, NT: int, NP: int, D0: float, dt: float) -> tuple:
 
     simulation = Simulation(NT, NP, D0, dt)
     simulation_data = simulation.run(i).get()
+    _, _, diffusion_tensor, eigen_diffusion_tensor, fa = simulation_data
 
-    contents += (f'Simulation Details:\nNT: {NT}\n'
-                 'NP: {NP}\n'
-                 'D0: {D0}\n'
-                 'dt: {dt}\n\n'
-                 'Diffusion Tensor:\n{diffusion_tensor}\n\n'
-                 'Diffusion Tensor Eigen Values:\n{eigen_diffusion_tensor}\n\n'
-                 'Fractional Anisotropy:\n{fa}\n')
+    contents += (f'Simulation Details:\nNT: {NT}\nNP: {NP}\nD0: {D0}\ndt: {dt}\n\nDiffusion Tensor:\n{diffusion_tensor}\n\nDiffusion Tensor Eigen Values:\n{eigen_diffusion_tensor}\n\nFractional Anisotropy:\n{fa}\n')
 
     end = time.time()
 
@@ -93,7 +88,7 @@ if __name__ == '__main__':
     D0 = 2.3 * 10**(-3) # diffusion coefficient
     dt = 5 * 10**(-9)   # time step
 
-    NP = [10, 30, 100, 300, 1000, 3000, 10000, 30000, 100000, 300000]
+    NP = [10, 30, 100, 300, 1000, 3000, 10000, 30000]
 
     # TODO(justin): simulation related functions should be in their own module...
     simulation = simulate_on_multiple_cores(NT, NP, D0, dt, repeats=3)
