6d45b62a7c3680eb3d738367caf6dcd598acbc15
justinac0 <justinac.1999@gmail.com>
2025-06-17 18:55:36 +1000

    working on updating

--- src/simulation.py
+++ src/simulation.py
@@ -9,5 +9,4 @@ if __name__ == '__main__':
     NP = smath.equidistant_np_space(ic.min_particles, ic.max_particles, ic.iterations)
     elapsed_time, simulations = parallel.simulate_on_multiple_cores(ic.NT, NP, ic.D0, ic.dt, ic.repeats)
     yamlhelper.write_simulations_files(ic.NT, ic.D0, ic.dt, simulations, ic.repeats, elapsed_time)
-    playsound('resources/audio/water_drop_reverb.mp3')
 
